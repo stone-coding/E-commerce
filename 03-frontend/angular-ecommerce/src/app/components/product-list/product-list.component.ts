3437af8cc4e18d1c1e526c03d0b31b5df55b70bb
stone-coding <hxs649@case.edu>
2023-08-14 22:24:11 -0400

    done shopping cart status component

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -29,6 +31,7 @@ export class ProductListComponent implements OnInit {
   //which is for accessing route parameters
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -138,7 +141,12 @@ export class ProductListComponent implements OnInit {
     }
   }
 
+  // call CartService.ts 
   addToCart(theProduct: Product) {
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
 }
---
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -0,0 +1,90 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = []
+
+  //subject is a subclass of observable
+  //we can use subject to publish events in our code
+  //The event will be sent to all of the subscribers
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem){
+    //check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined!;
+  
+
+    
+    if (this.cartItems.length > 0) {
+      //find the item in the cart based on item id
+
+      for(let tempCartItem of this.cartItems) {
+        if(tempCartItem.id == theCartItem.id) {
+          existingCartItem = tempCartItem
+          break;
+        }
+      }
+
+       //check if we found it 
+       alreadyExistsInCart =(existingCartItem != undefined)
+
+    }
+    if(alreadyExistsInCart){
+      //increment the quantity
+      existingCartItem.quantity++;
+     }
+     else {
+      //just add the item to the array
+      this.cartItems.push(theCartItem)
+     }
+
+     //compute cart total price and total quantity
+     this.computeCartTotals();
+      
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems){
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    //publish the new values ... all subscribers will receive the new data
+    //next will push events to all subscribers, and one event for totalPrice and the
+    // other for totalQuantity
+    this.totalPrice.next(totalPriceValue)
+    this.totalQuantity.next(totalQuantityValue)
+
+    //log cart data just for debugging purpose
+    this.logCartData(totalPriceValue, totalQuantityValue)
+
+  }
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log(`Contents of the cart`);
+    for(let tempCartItem of this.cartItems){
+      const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice
+      console.log(`name:${tempCartItem.name},
+       quantity=${tempCartItem.quantity}, 
+       unitPrice=${tempCartItem.unitPrice}, 
+       subTotalPrice=${subTotalPrice}`);
+    }
+
+    console.log(`totalPrice:${totalPriceValue.toFixed(2)}, totalQuantity:${totalQuantityValue}`);
+    console.log('---');
+    
+    
+    
+  }
+}
